fa866b013a41ebc572703bd265dc71c3fc5a4da9
asmburning <826102786@qq.com>
2016-11-11 09:55:07 +0800

    complete sftp

--- src/main/java/org/liuxinyi/utils/sftp/SftpUtil.java
+++ src/main/java/org/liuxinyi/utils/sftp/SftpUtil.java
@@ -5,9 +5,20 @@ import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.liuxinyi.utils.vo.FileVo;
+import org.liuxinyi.utils.vo.TransferResult;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+import java.util.Vector;
 
 /**
  * 不适用大规模sftp传输
@@ -44,32 +55,160 @@ public class SftpUtil {
      * 创建sftp目录
      *
      * @param channelSftp
-     * @param directory
+     * @param dir
      * @return
      * @throws
      */
-    public static ChannelSftp mkSftpDir(ChannelSftp channelSftp, String directory) {
-        if (directory.startsWith("/")) {
-            directory = directory.substring(1);
+    public static boolean mkSftpDir(ChannelSftp channelSftp, String dir) {
+        if (dir.startsWith("/")) {
+            dir = dir.substring(1);
         }
-        if (directory.endsWith("/")) {
-            directory = directory.substring(0, directory.length() - 1);
+        if (dir.endsWith("/")) {
+            dir = dir.substring(0, dir.length() - 1);
         }
-        if (StringUtils.isBlank(directory)) {
-            return channelSftp; //不需要创建目录直接返回
+        boolean result = false;
+        if (StringUtils.isBlank(dir)) {
+            return result; //不需要创建目录直接返回
         }
         try {
-            channelSftp.cd(directory);
+            channelSftp.cd(dir);
+            return true;
         } catch (Exception e) {
             try {
-                channelSftp.mkdir(directory);
-                channelSftp.cd(directory);
+                channelSftp.mkdir(dir);
+                channelSftp.cd(dir);
+                result = true;
             } catch (Exception e1) {
-                throw new RuntimeException(e1.getMessage());
+                log.error("failed to mkDir : {} ", dir, e);
             }
         }
-        return channelSftp;
+        return result;
     }
 
+    public static boolean changeDir(ChannelSftp channelSftp, String dir) {
+        try {
+            channelSftp.cd(dir);
+            return true;
+        } catch (Exception e) {
+            log.error("failed to changeDir : {} ", dir, e);
+        }
+        return false;
+    }
+
+    /**
+     * 获取当前目录下的所有的文件名(不包含文件夹)
+     *
+     * @param channelSftp channelSftp
+     * @return fileNames if no such file empty List
+     */
+    public static List<String> getFileNames(ChannelSftp channelSftp) {
+        return getFileNames(channelSftp, "");
+    }
+
+    /**
+     * 获得所有特定后缀的文件名
+     *
+     * @param channelSftp channelSftp
+     * @param extension   extension eg: .txt  .xls
+     * @return fileNames
+     */
+    public static List<String> getFileNames(ChannelSftp channelSftp, String extension) {
+        List<String> fileNames = new ArrayList<>();
+        Vector vector = null;
+        try {
+            vector = channelSftp.ls("*" + extension);
+            if (CollectionUtils.isEmpty(vector)) {
+                log.error("没有符合条件的文件 : {} ", extension);
+                return fileNames;
+            }
+            for (int i = 0; i < vector.size(); i++) {
+                String lsResult = vector.get(i).toString();
+                if (lsResult.indexOf(".") > 0) {
+                    String fileName = lsResult.substring(lsResult.lastIndexOf(" ") + 1);
+                    log.info("vector String : {} , fileName : {} ", lsResult, fileName);
+                    fileNames.add(fileName);
+                }
+            }
+        } catch (Exception e) {
+            log.error("failed to get fileNames ", e);
+        }
+        return fileNames;
+    }
+
+    /**
+     * 批量下载文件
+     *
+     * @param channelSftp channelSftp
+     * @param fileVos     fileVos
+     * @return transferResults
+     */
+    public static List<TransferResult> downloadFiles(ChannelSftp channelSftp, FileVo... fileVos) {
+        List<TransferResult> transferResults = new ArrayList<>();
+        for (FileVo fileVo : fileVos) {
+            transferResults.add(downloadFile(channelSftp, fileVo));
+        }
+        return transferResults;
+    }
+
+    /**
+     * 单个下载文件
+     *
+     * @param channelSftp channelSftp
+     * @param fileVo      fileVo
+     * @return
+     */
+    public static TransferResult downloadFile(ChannelSftp channelSftp, FileVo fileVo) {
+        OutputStream outputStream = null;
+        String message = "";
+        try {
+            outputStream = new FileOutputStream(fileVo.getLocalFilePath());
+            channelSftp.get(fileVo.getRemoteName());
+            return new TransferResult(fileVo);
+        } catch (Exception e) {
+            message = e.getMessage();
+            log.error("failed to downloadFile : {} ", fileVo, e);
+        } finally {
+            IOUtils.closeQuietly();
+        }
+        return new TransferResult(fileVo, message);
+    }
+
+    /**
+     * 批量上传文件
+     *
+     * @param channelSftp channelSftp
+     * @param fileVos     fileVos
+     * @return transferResults
+     */
+    public static List<TransferResult> uploadFiles(ChannelSftp channelSftp, FileVo... fileVos) {
+        List<TransferResult> transferResults = new ArrayList<>();
+        for (FileVo fileVo : fileVos) {
+            transferResults.add(uploadFile(channelSftp, fileVo));
+        }
+        return transferResults;
+    }
+
+    /**
+     * 单个上传文件
+     *
+     * @param channelSftp channelSftp
+     * @param fileVo      fileVo
+     * @return transferResult
+     */
+    public static TransferResult uploadFile(ChannelSftp channelSftp, FileVo fileVo) {
+        InputStream inputStream = null;
+        String message = "";
+        try {
+            inputStream = new FileInputStream(fileVo.getLocalFilePath());
+            channelSftp.put(inputStream, fileVo.getRemoteName());
+            return new TransferResult(fileVo);
+        } catch (Exception e) {
+            message = e.getMessage();
+            log.error("failed to uploadFile : {} ", fileVo, e);
+        } finally {
+            IOUtils.closeQuietly();
+        }
+        return new TransferResult(fileVo, message);
+    }
 
 }
