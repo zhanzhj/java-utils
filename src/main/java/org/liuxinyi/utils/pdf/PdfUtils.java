613e31b7479e09f966a821c9b91c4d2c25758c0f
asmburning <826102786@qq.com>
2016-11-29 15:47:37 +0800

    居中,添加图片

--- src/main/java/org/liuxinyi/utils/pdf/PdfUtils.java
+++ src/main/java/org/liuxinyi/utils/pdf/PdfUtils.java
@@ -1,8 +1,6 @@
 package org.liuxinyi.utils.pdf;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Font;
-import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfWriter;
 import lombok.extern.slf4j.Slf4j;
@@ -26,8 +24,13 @@ public class PdfUtils {
             document.open();
             BaseFont baseFont = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
             Font font = new Font(baseFont, 12, Font.NORMAL);
-            document.add(new Paragraph("PDF 中文测试", font));
-            // document.add(new Paragraph("PDF"));
+            //font.setColor(5, 5, 5);
+            Paragraph paragraph = new Paragraph("PDF 中文测试", font);
+            paragraph.setAlignment(Element.ALIGN_CENTER); // 设置居中
+            document.add(paragraph);
+            Image image = Image.getInstance("C:/tmp/qr/logo1.jpg");
+            image.scaleAbsolute(200, 150);
+            document.add(image);
             document.close();
         } catch (Exception e) {
             log.error("failed to create pdf!", e);
